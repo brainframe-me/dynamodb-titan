f2e1456466c8cac83821421edfbbf55f85d99a0a
Cox Davy <dcox@brainframe.lu>
2015-11-13 17:45:03 +0100

    Initial version

--- Vagrantfile_bootstrap.sh
+++ Vagrantfile_bootstrap.sh
@@ -23,4 +23,6 @@ docker run -t --name dynamodb-local -p 8000:8000 brainframe/dynamodb-local &
 docker run -t --name titan-on-dynamodb --link dynamodb-local:dynamodb-local -p 8182:8182 -p 8183:8183 -p 8184:8184 -e DYNAMODB_HOSTPORT=http://dynamodb-local:8000 -e AWS_ACCESS_KEY_ID=notcheckedlocallybutmustbeprovided -e AWS_SECRET_ACCESS_KEY=notcheckedlocallybutmustbeprovided -e GRAPH_NAME=yourdatabasename brainframe/titan-on-dynamodb &
 
 # Background message
-echo "If this is the first time you do the provision, the dockers are being downloaded in the background which might take a while (+-2GB). Once they have been loaded locally, a new 'vagrant provision' will go quickly"
+echo "If this is the first time you do the provision, the dockers are being downloaded in the background which might take a while (+-2GB)."
+echo "They will only start after the download of the images is completed, so count on 2-5 minutes before testing"
+echo "Once the images are in vagrant, a new 'vagrant provision' will go quickly and immediately start the dockers"
