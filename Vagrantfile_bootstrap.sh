408473733b1f5d25eb2fc1f8a0c6d49acf881b3e
Cox Davy <dcox@brainframe.lu>
2015-11-13 19:30:19 +0100

    Initial version

--- Vagrantfile_bootstrap.sh
+++ Vagrantfile_bootstrap.sh
@@ -17,10 +17,12 @@ sudo rsync -avu --filter=':- /vagrant/.gitignore' /vagrant/ /local/
 sudo chown -R vagrant:vagrant /local
 
 # Start docker for dynamodb-local
-docker run -t --name dynamodb-local -p 8000:8000 brainframe/dynamodb-local &
+echo "downloading and starting brainframe/dynamodb-local..."
+docker run -d --name dynamodb-local -p 8000:8000 brainframe/dynamodb-local
 
 # Start docker for titan-on-dynamodb
-docker run -t --name titan-on-dynamodb --link dynamodb-local:dynamodb-local -p 8182:8182 -p 8183:8183 -p 8184:8184 -e DYNAMODB_HOSTPORT=http://dynamodb-local:8000 -e AWS_ACCESS_KEY_ID=notcheckedlocallybutmustbeprovided -e AWS_SECRET_ACCESS_KEY=notcheckedlocallybutmustbeprovided -e GRAPH_NAME=yourdatabasename brainframe/titan-on-dynamodb &
+echo "downloading and starting brainframe/titan-on-dynamodb..."
+docker run -d --name titan-on-dynamodb --link dynamodb-local:dynamodb-local -p 8182:8182 -p 8183:8183 -p 8184:8184 -e DYNAMODB_HOSTPORT=http://dynamodb-local:8000 -e AWS_ACCESS_KEY_ID=notcheckedlocallybutmustbeprovided -e AWS_SECRET_ACCESS_KEY=notcheckedlocallybutmustbeprovided -e GRAPH_NAME=yourdatabasename brainframe/titan-on-dynamodb &
 
 # Background message
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
